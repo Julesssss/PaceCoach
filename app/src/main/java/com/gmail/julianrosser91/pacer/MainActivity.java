ab1b6e6708330c9937bf20195e527fe25f5e8d9e
Julian Rosser <j.rosser@replyltd.co.uk>
2016-06-25 19:29:18 +0100

    Implemented FusedLocationProvider. Connecting, listening and displaying the returned location.

--- app/src/main/java/com/gmail/julianrosser91/pacer/MainActivity.java
+++ app/src/main/java/com/gmail/julianrosser91/pacer/MainActivity.java
@@ -3,24 +3,49 @@ package com.gmail.julianrosser91.pacer;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.location.GpsStatus;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    // Constants
+    private static final int MILLISECONDS_PER_SECOND = 1000;
+    public static final int UPDATE_INTERVAL_IN_SECONDS = 1;
+    private static final long UPDATE_INTERVAL = MILLISECONDS_PER_SECOND
+            * UPDATE_INTERVAL_IN_SECONDS;
+    private static final int FASTEST_INTERVAL_IN_MILLISECONDS = 100;
+    private final static int LOCATION_PERMISSION_REQUEST = 100;
+    final static public SimpleDateFormat DATE_FORMAT_LAST_UPDATED = new SimpleDateFormat("hh:m.s");
+
+    // Views
+    private TextView mTextCurrentLocation;
+    private TextView mTextLastUpdated;
+
+    // Objects
     public Context mContext;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private Location mCurrentLocation;
+    private String mLastUpdateTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,18 +54,40 @@ public class MainActivity extends AppCompatActivity {
         mContext = this;
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Log.i(getClass().getSimpleName(), "onCreate");
 
+        mTextCurrentLocation = (TextView) findViewById(R.id.text_current_location);
+        mTextLastUpdated = (TextView) findViewById(R.id.text_last_updated);
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        mLocationRequest = createLocationRequest();
     }
 
-    public void askForLocationPermission() {
-        ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                100);
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
     }
 
 
-    public  void checkLocationPermission() {
+    protected LocationRequest createLocationRequest() {
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL_IN_MILLISECONDS);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        return mLocationRequest;
+    }
+
+    public void checkLocationPermission() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -69,6 +116,59 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.i(getClass().getSimpleName(), "onGoogleAPiConnected");
+//        if (mRequestingLocationUpdates) {
+        startLocationUpdates();
+//        }
+    }
+
+    protected void startLocationUpdates() {
+        // Permission check
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            askForLocationPermission();
+            return;
+        }
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, new com.google.android.gms.location.LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        handleUpdatedLocation(location);
+                    }
+                });
+    }
+
+    private void handleUpdatedLocation(Location location) {
+        mCurrentLocation = location;
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+        Log.i(getClass().getSimpleName(), "new loc: " + location.getLatitude() + " | " + location.getLongitude());
+        System.out.print("new loc: " + location.getLatitude() + " | " + location.getLongitude());
+
+        mTextCurrentLocation.setText(location.getLatitude() + " | " + location.getLongitude());
+        mTextLastUpdated.setText(DATE_FORMAT_LAST_UPDATED.format(new Date()));
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(getClass().getSimpleName(), "onConnectionSuspended");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i(getClass().getSimpleName(), "onConnectionFailed");
+    }
+
+    public void askForLocationPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                LOCATION_PERMISSION_REQUEST);
     }
 
 }
