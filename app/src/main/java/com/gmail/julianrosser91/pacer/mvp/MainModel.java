bea91be8e1b9741087e82aa17deb2f53034e6777
Julian Rosser <j.rosser@replyltd.co.uk>
2016-09-18 00:53:48 +0100

    Improved fake pace calculation to better represent split pace. Prevent Handler from running multiple Threads & protection NPE's.

--- app/src/main/java/com/gmail/julianrosser91/pacer/mvp/MainModel.java
+++ app/src/main/java/com/gmail/julianrosser91/pacer/mvp/MainModel.java
@@ -1,14 +1,18 @@
 package com.gmail.julianrosser91.pacer.mvp;
 
-import android.widget.Toast;
+import android.os.Handler;
 
+import com.gmail.julianrosser91.pacer.utils.PaceUtils;
 import com.tinmegali.mvp.mvp.GenericModel;
+
+import java.util.Random;
 
 public class MainModel extends GenericModel<MVPInterfaces.RequiredPresenterOps>
         implements MVPInterfaces.ProvidedModelOps {
 
     // Presenter reference
     private MVPInterfaces.RequiredPresenterOps mPresenter;
+    private Handler mHandler;
 
     public MainModel(MVPInterfaces.RequiredPresenterOps mPresenter) {
         this.mPresenter = mPresenter;
@@ -41,10 +45,84 @@ public class MainModel extends GenericModel<MVPInterfaces.RequiredPresenterOps>
     @Override
     public void startTrackingService() {
         mPresenter.onTrackingServiceStarted();
+        startReturningFakeLocationData();
     }
 
     @Override
     public void stopTrackingService() {
         mPresenter.onTrackingServiceStopped();
+        stopRepeatingTask();
+    }
+
+    /**
+     * todo - Temporary method for supplying sample location data
+     */
+    private void startReturningFakeLocationData() {
+        if (mHandler != null) {
+            mHandler.removeCallbacks(fakePaceGenerater);
+        } else {
+            mHandler = new Handler();
+        }
+        startRepeatingTask();
+    }
+
+    private void generateFakePace() {
+        int r = new Random().nextInt(20);
+        double v = new Random().nextDouble() * 10;
+
+        long meters = (long) (v + r);
+        Split split = new Split(meters, 3);
+        mPresenter.onLocationUpdated(split);
+    }
+
+    void startRepeatingTask() {
+        fakePaceGenerater.run();
+    }
+
+    void stopRepeatingTask() {
+        if (mHandler != null) {
+            mHandler.removeCallbacks(fakePaceGenerater);
+        }
+    }
+
+    Runnable fakePaceGenerater = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                generateFakePace();
+            } finally {
+                // 100% guarantee that this always happens, even if
+                // your update method throws an exception
+                mHandler.postDelayed(fakePaceGenerater, 3000);
+            }
+        }
+    };
+
+
+    public class Split {
+
+        long seconds;
+        long meters;
+
+        public Split(long meters, long seconds) {
+            this.seconds = seconds;
+            this.meters = meters;
+        }
+
+        public long getSeconds() {
+            return seconds;
+        }
+
+        public long getMeters() {
+            return meters;
+        }
+
+        public String getKmPerHour() {
+            return "" + PaceUtils.getKmPerHour(seconds * 1000, meters) + " kmph";
+        }
+
+        public String getPace() {
+            return meters / seconds + "/mps   //  " + seconds + " secs / " + meters + " meters";
+        }
     }
 }
